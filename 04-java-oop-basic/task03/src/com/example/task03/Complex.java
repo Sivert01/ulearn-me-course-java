fd13f38abc7a9d351f0ce6c3afc3eec3d7d3fc5c
Sivert01 <dodab@mail.ru>
2020-12-25 04:05:03 +0500

    Сиверт Дмитрий Задача 4.3

---
+++ 04-java-oop-basic/task03/src/com/example/task03/Complex.java
@@ -0,0 +1,51 @@
+package com.example.task03;
+
+public class Complex {
+    private final int re;
+    private final int im;
+
+    public Complex(int re, int im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public int getRe(){
+        return re;
+    }
+
+    public int getIm() {
+        return im;
+    }
+
+    public Complex add(Complex complex) {
+        int newRe = this.re + complex.re;
+        int newIm = this.im + complex.im;
+        return new Complex(newRe, newIm);
+    }
+
+    public Complex multiply(Complex complex) {
+        int newRe = this.re * complex.re - this.im * complex.im;
+        int newIm = this.im * complex.re + this.re * complex.im;
+        return new Complex(newRe, newIm);
+    }
+
+    public String toString() {
+        String sign = " + ";
+        int imForPrint = this.im;
+        if (Integer.toString(this.im).charAt(0) == '-') {
+            sign = " - ";
+            imForPrint *= -1;
+        }
+        return this.re + sign + imForPrint + "i";
+    }
+}
+
+class Task03Main {
+    public static void main(String[] args) {
+
+        Complex complex = new Complex(2, -5);
+        Complex newComplex = new Complex(4, -7);
+        System.out.println(complex.add(newComplex).toString());
+        System.out.println(complex.multiply(newComplex).toString());
+    }
+}
